ffe4cbe343efc4581d855d103f2292be8a8aeb8b
Asrat23 <asratendale23@gmail.com>
2023-01-03 08:33:15 -0800

    'first'

---
+++ back_end/serializers.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Student_information
+
+
+class studentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student_information
+        fields = '__all__'
